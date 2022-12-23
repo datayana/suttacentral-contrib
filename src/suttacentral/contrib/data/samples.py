b6eacc9a69ea2b887b6cc5293831fcf64fa6465a
TheraByte <therabyte@datayana.info>
2022-12-23 14:30:11 -0800

    correct catchphrase

--- src/suttacentral/contrib/data/samples.py
+++ src/suttacentral/contrib/data/samples.py
@@ -1,5 +1,5 @@
 PALI_CATCHPHRASES = [
-    "Evaṃ me sutaṃ",
+    "Evaṁ me sutaṁ",
     "Yaṁ kiñci samudayadhammaṁ sabbaṁ taṁ nirodhadhamman.",
     "Etha tumhe, kālāmā, mā anussavena, mā paramparāya, mā itikirāya, mā piṭakasampadānena, mā takkahetu, mā nayahetu, mā ākāraparivitakkena, mā diṭṭhinijjhānakkhantiyā, mā bhabbarūpatāya, mā samaṇo no garūti. Yadā tumhe, kālāmā, attanāva jāneyyātha: ‘ime dhammā akusalā, ime dhammā sāvajjā, ime dhammā viññugarahitā, ime dhammā samattā samādinnā ahitāya dukkhāya saṁvattantī’ti, atha tumhe, kālāmā, pajaheyyātha.",
 ]
